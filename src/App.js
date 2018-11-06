67d8561bd9207359880c6eeb74cc4a78226ff851
Chris Roman <Christopherroman85@gmail.com>
2018-11-06 10:32:22 -0600

    working on frontend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Login from './Login'
+import ShowUser from './ShowUser'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Login />
+        {this.state.logged ? <ShowUser handleLogin={this.handleLogin} username={this.state.username}/> : <Login handleLogin={this.handleLogin} />}
       </div>
     );
   }
---
+++ src/ShowUser/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import MovieContainer from '../MovieContainer';
+
+const Movies = (props) => {
+	const movies = props.movies.map
+}
