4c333686e6679aed3b9afcbb3bdef785fce7058d
Filip Tatarnik <ftatarnik@gmail.com>
2018-11-06 15:56:19 -0600

    more semantic UI

--- src/ShowUser/index.js
+++ src/ShowUser/index.js
@@ -5,29 +5,37 @@ import { Grid, Card, Container, Header  } from 'semantic-ui-react';
 class User extends Component {
 	render() {
 		return(
-		<Container >
-			<Card>
+		<div class="movie-container">
+			<Container>
 				<Card.Content>
 
-					<Header as="h1">Hello, {this.props.username}!</Header>
+					<Header as="h1">Hey {this.props.username}!</Header>
+			  <img src='https://react.semantic-ui.com/images/avatar/small/matthew.png' class='ui-image' />
+			  	{/* this is a place holder for the users image if they so choose to put one*/}
 
 
 				<Card.Header>
-				
+
 					Watch List!
-				
+				<Card.Content>
+				This is where the users watch list is going to go
+				</Card.Content>
 				</Card.Header>
 				<br/>
 				<Card.Header>
 				
 					Fav Movies!
-				
+				<Card.Content>
+				This is where the users fav movies list is going to go
+				</Card.Content>
 				</Card.Header>
 				<br/>
 				<Card.Header>
 				
 					Owned Movies
-				
+				<Card.Content>
+				This is where the users owned movie list is going to go
+				</Card.Content>
 				</Card.Header>
 				<br/>
 				
@@ -40,8 +48,8 @@ class User extends Component {
 
 				</Card.Content>
 			
-			</Card>
-		</Container>
+			</Container>
+		</div>
 		)
 	}
 }
