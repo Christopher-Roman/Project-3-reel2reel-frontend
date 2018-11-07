3f3dc2c6eb49f31be8837d21daee60b282fa2c34
Chris Roman <Christopherroman85@gmail.com>
2018-11-07 17:24:54 -0600

    search function works

--- src/ShowUser/index.js
+++ src/ShowUser/index.js
@@ -39,53 +39,47 @@ class ShowUser extends Component {
 	render() {
 		return(
 			<Grid textAlign="center">
-				<div class="movie-container">
+				<div className="movie-container">
 					<Container >
-
+						<Grid textAlign="center">
 							<Card.Content>
-						<Header as="h1">Hey {this.props.username}!</Header>
-						<img src='https://react.semantic-ui.com/images/avatar/small/matthew.png' class='ui-image' />
+								<Header as="h1">Hey {this.props.username}!</Header>
+								<img src='https://react.semantic-ui.com/images/avatar/small/matthew.png' className='ui-image' />
 							</Card.Content>
-						<Card>
-							<Card.Header>
-								Watch List!
-							</Card.Header>
-
-							<Card.Content>
-							This is where the users watch list is going to go
-							</Card.Content>
-
+							<Card>
+								<Card.Header>
+									Watch List!
+								</Card.Header>
+								<Card.Content>
+								This is where the users watch list is going to go
+								</Card.Content>
 								<WatchList watchList={this.state.watchList}/>
-						</Card>	
-
-						<Card>
-							<Card.Header>
-								Fav Movies!
-							</Card.Header>
-
-							<Card.Content>
-							This is where the users fav movies list is going to go
-							</Card.Content>
+							</Card>	
+							<Card>
+								<Card.Header>
+									Fav Movies!
+								</Card.Header>
+								<Card.Content>
+									This is where the users fav movies list is going to go
+								</Card.Content>
 								<FavMovies favMovies={this.state.favMovies}/>
-						</Card>
-
-						<Card>
-							<Card.Header>
-								Owned Movies
-							</Card.Header>
-							<Card.Content>
-							This is where the users owned movie list is going to go
-							</Card.Content>
+							</Card>
+							<Card>
+								<Card.Header>
+									Owned Movies
+								</Card.Header>
+								<Card.Content>
+									This is where the users owned movie list is going to go
+								</Card.Content>
 								<OwnedMovies ownedMovies={this.state.ownedMovies}/>
-						</Card>
-
-						<Card>				
-							<Card.Header>
-								<MovieContainer />
-							<br/>
-							</Card.Header>
-						</Card>
-
+							</Card>
+							<Card>				
+								<Card.Header>
+									<MovieContainer />
+									<br/>
+								</Card.Header>
+							</Card>
+						</Grid>
 					</Container>
 				</div>
 			</Grid>
