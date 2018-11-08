90f2bcb56695ccd116cb6eba796429d43d9b02f7
Chris Roman <Christopherroman85@gmail.com>
2018-11-08 13:22:38 -0600

    modal is displaying information

--- src/MovieContainer/index.js
+++ src/MovieContainer/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ShowMovie from '../ShowMovie'
 import { Search, Button, Form, Input, Grid } from 'semantic-ui-react';
 
 
@@ -7,9 +8,15 @@ class MovieContainer extends Component {
 		super();
 
 		this.state = {
-			search: ''
+			search: '',
+			searchedMovie: {
+				title: '',
+				release_date: '',
+				id: '',
+				poster_400x570: ''
+			},
+			showSearchModal: false
 		}
-		showSearchModal: false
 	}
 	searchMovie = async (e) => {
 		e.preventDefault();
@@ -17,6 +24,11 @@ class MovieContainer extends Component {
 		// fetch call to your backend localhost:5000?movie=terminator
 		const searchResult = await fetch('http://localhost:9000/movie/search?searchTerm=' + this.state.search)
 		const foundMovie = await searchResult.json();
+		this.setState({
+			searchedMovie: {
+				...foundMovie.results[0]
+			}
+		})
 		console.log('here is foundMovie');
 		console.log(foundMovie); // id is in here
 		// return searchResult;
@@ -26,14 +38,26 @@ class MovieContainer extends Component {
 			search: e.currentTarget.value
 		})
 	}
+	openModal = (moviesSearched) => {
+            console.log(moviesSearched, 'moviesSearched-------------')
+
+            this.setState({
+            showMovieModal: true,
+                moviesSearched: {
+                    ...moviesSearched
+                }
+            })
+    }
     render(){
+    	console.log(this.state.searchedMovie);
         return(
 
         	<div className="movie-info">
 	        	<Grid columns={1} divided textAlign='center' style={{ height: '100%' }} stackable>
 		            <Form onSubmit={this.searchMovie}><br/>Find a Movie <br /><br />
 						<Input type="text" name="search" value={this.state.search} onChange={this.handleChange} /> <br /><br />
-					<Button color="green">search</Button>
+						<Button color="green" onClick={this.openModal}>search</Button>
+						<ShowMovie open={this.state.showMovieModal} searchedMovie={this.state.searchedMovie}/>
 					</Form>
 				</Grid>
 			</div>
--- src/ShowMovie/index.js
+++ src/ShowMovie/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import MovieContainer from '../MovieContainer'
 import { Modal, Button, Header, Form, Label} from 'semantic-ui-react';
 
 const ShowMovie = (props) => {
+	console.log(props.searchedMovie);
 	return(
 		<Modal open = {props.open}>
 			<Header>Show Movie</Header>
 			<Modal.Content>
 				<Form>
+					<img src={props.searchedMovie.poster_400x570} /> <br /><br />
 					<Label>
-						Movie Title
-					</Label>
+						Movie Title: {props.searchedMovie.title}
+					</Label> <br /><br />
 					<Label>
-						Movie Description
+						Movie Release Date: {props.searchedMovie.release_date}
 					</Label>
+					<br />
+					<br />
 					<Modal.Actions>
 						<Button type="submit">Add Movie to FavList
 						</Button>
